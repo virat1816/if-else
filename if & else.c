c8f4c998312d48afa5028a5f09cdf2a2e07ebd4e
virat1816 <116003414+virat1816@users.noreply.github.com>
2023-02-21 11:29:38 +0530

    Add files via upload

---
+++ if & else.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+ main(){
+ 	
+    int num;
+    
+    printf("Enter Number: ");
+    scanf("%d",&num);
+ 
+    if(num == 0)
+    	 printf("This Number positive nor negative");
+	else if(num < 0)
+		printf("Negative");
+	else
+		printf("Positive");
+	
+}
